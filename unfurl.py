9ff266d78bbe49065a9fa08621be884a1cce8b4c
Deep Kapadia <deep@deepkapadia.com>
2013-12-09 02:32:27 -0500

    added with hops method. Very hacky, need to replicate cookiejar functionality to store cookie in memory and call with all the cookies as a part of the header

--- unfurl.py
+++ unfurl.py
@@ -1,6 +1,9 @@
 import urllib2
 import cookielib
 from urlparse import urlparse
+import httplib2 as httplib
+import json
+
 
 def expand_url(url):
 	#set cookie since many sites *WRONGLY* require you to set a cookie to redirect you to a secure page and back to insecure content
@@ -21,4 +24,29 @@ def expand_url(url):
 	#open url
 	opened_url = opener.open(parsed_url)
 	resolved_url = opened_url.geturl()
+	#print cookiejar
 	return resolved_url
+
+
+def expand_url_with_hops(url, hops = [], headers = None):
+    expanded = expand_url(url)
+    parsed = urlparse(url)
+    cookiejar = cookielib.CookieJar()
+    h = httplib.Http()
+    h.follow_redirects = False
+    response, content = h.request(url, 'HEAD', headers)
+    if response.has_key('set-cookie'):
+    	#if headers is None:
+    	headers = json.dumps({'Cookie': response['set-cookie']})
+    	#else:
+    	#	headers += " "+response['set-cookie']
+    #print headers+"\n"
+    if response.status/100 == 3 and response['location']:
+    	redirected_url = response['location']
+    	hops.append(redirected_url)
+    	if expanded == redirected_url:
+            return url,hops
+        return expand_url_with_hops(redirected_url, hops, headers) # changed to process chains of short urls
+    else:
+        return url, hops
+
